36d34adfd0e82f7f22c0bfe5207458588aefe31f
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-02-23 14:49:11 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Demira Pasalic and is{" "}
-        <a
-          href="https://github.com/Demira11/official-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Demira Pasalic and is{" "}
+          <a
+            href="https://github.com/Demira11/official-react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
